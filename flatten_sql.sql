bf77c3046783b6fc342fee52f979522dd2c86901
edu-spot <eeduspot@gmail.com>
2025-07-15 16:29:46 +0530

    Create flatten_sql.sql

---
+++ flatten_sql.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE PROCEDURE flatten_table_with_dynamic_attributes()
+RETURNS TABLE ()
+LANGUAGE SQL
+AS
+$$
+DECLARE
+  dynamic_sql STRING;
+  column_list STRING;
+BEGIN
+  -- Extract dynamic keys from attributes
+  WITH keys AS (
+    SELECT DISTINCT f.key
+    FROM my_table,
+         LATERAL FLATTEN(input => attributes) f
+  )
+  SELECT LISTAGG('attributes:"' || key || '" AS "' || key || '"', ', ') INTO :column_list
+  FROM keys;
+
+  -- Construct full SQL
+  LET dynamic_sql = '
+    SELECT 
+      t.id,
+      flattened_id.value::STRING AS individual_id,
+      ' || column_list || '
+    FROM my_table t,
+         LATERAL FLATTEN(input => t.ids) AS flattened_id
+  ';
+
+  -- Return the dynamic query
+  RETURN TABLE(EXECUTE IMMEDIATE :dynamic_sql);
+END;
+$$;
